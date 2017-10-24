9c01964ce0f151246658b898526b8a064f5b7837
Nathan Smith <n.smith167@gmail.com>
2017-10-24 18:37:14 -0400

    Public API finished

--- PhotoCollect/src/Application.java
+++ PhotoCollect/src/Application.java
@@ -3,9 +3,11 @@
  * @author nps5120
  */
 public class Application {
-    
+
     public static void main(String[] args)
     {
+        Controller controller = new Controller();
         System.out.println("Main class initialized.");
     }
+    
 }
