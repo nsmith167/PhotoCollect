e02c2c61dfab3c259a0938988cc19d6cbf73e44d
nps5120 <nps5120@WGATEE206-106.win.psu.edu>
2017-10-24 12:40:06 -0400

    Created initial Java project

---
+++ PhotoCollect/src/PhotoCollect.java
@@ -0,0 +1,11 @@
+/**
+ * Main application class 
+ * @author nps5120
+ */
+public class PhotoCollect {
+    
+    public static void main(String[] args)
+    {
+        System.out.println("Main class initialized.");
+    }
+}
