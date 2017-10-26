50557adf775bf9fff21e31c6a9282619404b115f
nps5120 <nps5120@WGATEE206-106.win.psu.edu>
2017-10-26 13:17:56 -0400

    Added some Collection functionality.

--- PhotoCollect/src/Collection.java
+++ PhotoCollect/src/Collection.java
@@ -1,5 +1,5 @@
-
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Represents a collection of photos and associated data
@@ -9,15 +9,29 @@ public class Collection {
     
     private String title;
     private ArrayList<Item> items = new ArrayList<>();
+    private int totalItems;
+    private int totalValue;
+    private double avgRating;
+    private Date collectionStartDate;
+    private String latestItem;
     
     public Collection(String title)
     {
         this.title = title;
+        this.collectionStartDate = new Date();
+        this.totalItems = 0;
+        this.totalValue = 0;
+        this.avgRating = 0;
+        this.latestItem = "";
     }
     
     public void addItem(Item item)
     {
         items.add(item);
+        totalItems++;
+        totalValue += item.getValue();
+        avgRating = totalValue/totalItems;
+        this.latestItem = item.getItemName();
     }
     
     public void removeItem(Item item)
