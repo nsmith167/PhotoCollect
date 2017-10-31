ac439d0a24284275eabb483f9509a3f5716f564c
nps5120 <nps5120@WGATEE206-106.win.psu.edu>
2017-10-31 12:40:16 -0400

    Working on UI controls

--- PhotoCollect/src/Application.java
+++ PhotoCollect/src/Application.java
@@ -7,8 +7,6 @@ public class Application {
     public static void main(String[] args)
     {
         Controller controller = new Controller();
-        MainUI mui = new MainUI();
-        System.out.println("Main class initialized.");
     }
     
 }
